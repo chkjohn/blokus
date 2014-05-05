5e2e7c78a14a0030b58ef1a0a9c158de865870bd
chkjohn <chk09140813@gmail.com>
2014-05-05 16:40:42 +0800

    display gameboard

--- server.js
+++ server.js
@@ -100,6 +100,11 @@ var SampleApp = function() {
             res.send("<html><body><img src='" + link + "'></body></html>");
         };
 
+        self.routes['/'] = function(req, res) {
+            res.setHeader('Content-Type', 'text/html');
+            res.send(self.cache_get('index.html') );
+        };
+        
         self.routes['/index'] = function(req, res) {
             res.setHeader('Content-Type', 'text/html');
             res.send(self.cache_get('index.html') );
