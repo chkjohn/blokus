774b7583348db899c16d0df619bcc619f1e8446e
chkjohn <chk09140813@gmail.com>
2014-05-12 18:52:37 +0800

    add lightbox of confirming game room creation

--- js/waitingRoomView.js
+++ js/waitingRoomView.js
@@ -67,7 +67,7 @@ function init_waitingroom(socket){
 		
 		$('#confirmCreateGameRoom').click(function(){
 			$('#createGameRoomTable').css('display', 'none');
-			$('#confirmMessage').css('display', 'initial');
+			$('#confirmMessage').css({'display': 'initial', 'top': (window.innerHeight - 300)/2 + 'px', 'left': (window.innerWidth - 200)/2 + 'px'});
 			var name = $('#gameRoomName').val();
 			$('#gameRoomName').val('');
 			var message = $('<h1 align=center>Game Room ' + name + ' has been created.' + '</h1>');
