19d47ac7844a76b00f5ee847f9d06f015e066112
chkjohn <chk09140813@gmail.com>
2014-05-12 16:38:48 +0800

    building waiting room

--- js/waitingRoomView.js
+++ js/waitingRoomView.js
@@ -55,7 +55,7 @@ function init_waitingroom(socket){
 		});
 		
 		$('#createGameRoom').click(function(){
-			$('#createGameRoomTable').css({'display': 'initial', 'height': '300px', 'width': '300px', 'position': 'absolute', 'top': (window.innerHeight - 300)/2 + 'px', 'left': (window.innerWidth - 300)/2 + 'px', 'z-index': 5, 'background': 'white'});
+			$('#createGameRoomTable').css({'display': 'initial', 'height': '300px', 'width': '600px', 'position': 'absolute', 'top': (window.innerHeight - 300)/2 + 'px', 'left': (window.innerWidth - 300)/2 + 'px', 'z-index': 5, 'background': 'white'});
 			
 			$('#background').css({'display': 'initial', 'height': '100%', 'width': '100%', 'position': 'fixed', 'top': '0px', 'left': '0px', 'background': 'rgba(0,0,0,0.5)', 'z-index': 3});
 		});
